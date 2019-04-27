74839df56bb91a76aaf1d63583821350cefd9659
NelsonKioko <kiokonelson2@gmail.com>
2019-04-27 14:24:57 +0300

    --ammend

---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY='skrrrrrr'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://nelson:kioko@localhost/pitches'
+export MAIL_USERNAME='kiokonelson2@gmail.com'
+export MAIL_PASSWORD='Gizmal123'
+
+python3.6 manage.py server  
